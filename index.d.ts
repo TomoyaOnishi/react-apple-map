7e5effc1b800dd320ee08df0ba8245d625a58ad5
TomoyaOnishi <tomoyaonishi@icloud.com>
2019-06-17 09:22:24 +0900

    updated gitignore.

--- index.d.ts
+++
@@ -1,2 +0,0 @@
-export { testMethod } from './lib/typescript';
-export { TestComponent } from './lib/TestComponent';
--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var typescript_1 = require("./lib/typescript");
-exports.testMethod = typescript_1.testMethod;
-var TestComponent_1 = require("./lib/TestComponent");
-exports.TestComponent = TestComponent_1.TestComponent;
-//# sourceMappingURL=index.js.map
